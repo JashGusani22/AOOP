7544453472bcb13a70b14bbafbefab56437e3432
JashGusani22 <162081452+JashGusani22@users.noreply.github.com>
2025-04-03 15:43:50 +0530

    Create P21.java

---
+++ P21.java
@@ -0,0 +1,11 @@
+final class P21{
+    public void intro(){
+        System.out.println("This is final class");
+    }
+}
+public class Main{
+    public static void main(String[] args) {
+        P21 obj = new P21();
+        obj.intro();
+    }
+}
