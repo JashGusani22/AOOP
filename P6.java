c40764d072b3136a27b4a7cdb6b09c8dbf64bfd6
JashGusani22 <162081452+JashGusani22@users.noreply.github.com>
2025-04-03 15:11:43 +0530

    Create P6.java

---
+++ P6.java
@@ -0,0 +1,16 @@
+public class P6{
+   public int ernum;
+   public String name;
+   public Student(String name , int ernum){
+       this.ernum = ernum;
+       this.name = name;
+    }
+     public static void main(String[] args) {
+         Student s1 = new Student("Jash", 22);
+         Student s2 = new Student("Veer", 67);
+         Student s3 = new Student("Dip", 72);
+         System.out.println(s1.name);
+         System.out.println(s2.name); 
+         System.out.println(s3.name);
+     }
+}
