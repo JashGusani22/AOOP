ffa10db6c7468d725e4d7ff23dfcf01b88b2278f
JashGusani22 <162081452+JashGusani22@users.noreply.github.com>
2025-04-03 15:16:57 +0530

    Update P7.java

--- P7.java
+++ P7.java
@@ -1,12 +1,12 @@
  public class P7{
      double height;
      double weight;
-     public Rectangle(double height , double weight){
+     public P7(double height , double weight){
          this.height = height;
          this.weight = weight;
      }
     public static void main(String[] args) {
-      Rectangle r1 = new  Rectangle(34.50 , 65.40);
+      P7 r1 = new  P7(34.50 , 65.40);
         System.out.println(r1.height);
         System.out.println(r1.weight);
      }
