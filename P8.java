69952cbc3114d7a0ccd803c4ef56c5e472dd3737
JashGusani22 <162081452+JashGusani22@users.noreply.github.com>
2025-04-03 15:16:28 +0530

    Create P8.java

---
+++ P8.java
@@ -0,0 +1,13 @@
+ public class P8{
+     double h;
+     double w;
+     public P8(double h , double w){
+         this.height = h;
+         this.weight = w;
+     }
+    public static void main(String[] args) {
+      P8 r1 = new P8(34.20 , 65.20);
+        System.out.println(r1.h);
+        System.out.println(r1.w);
+     }
+ }
