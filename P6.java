9837351613c3d0b050f49edfbbd4e2af07d4673c
JashGusani22 <162081452+JashGusani22@users.noreply.github.com>
2025-04-03 15:19:18 +0530

    Update P6.java

--- P6.java
+++ P6.java
@@ -1,14 +1,14 @@
 public class P6{
    public int ernum;
    public String name;
-   public Student(String name , int ernum){
+   public P6(String name , int ernum){
        this.ernum = ernum;
        this.name = name;
     }
      public static void main(String[] args) {
-         Student s1 = new Student("Jash", 22);
-         Student s2 = new Student("Veer", 67);
-         Student s3 = new Student("Dip", 72);
+         P6 s1 = new Student("Jash", 22);
+         P6 s2 = new Student("Veer", 67);
+         P6 s3 = new Student("Dip", 72);
          System.out.println(s1.name);
          System.out.println(s2.name); 
          System.out.println(s3.name);
